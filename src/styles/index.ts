c17f905e52c06baa76886057ad41574063fcbb9d
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2021-04-30 11:12:58 +0200

    main: add fonts sizes, add emotion package

---
+++ src/styles/index.ts
@@ -0,0 +1,20 @@
+
+export type Fonts = {
+    family: "'Lato', sans-serif";
+    sizes: {
+      h1: "1.8rem";
+      h2: "1.6rem";
+      h3: "1.4rem";
+      h4: "1.2rem";
+      h5: "1.1rem";
+      base: "16px";
+      helperText: "0.8rem";
+      copyText: "0.7rem";
+    };
+    weights: {
+      light: 300;
+      regular: 400;
+      medium: 500;
+      semiBold: 600;
+    };
+  };
