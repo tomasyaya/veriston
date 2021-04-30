5c2195433b2a76f75a3074500a9f0a1d291a97f7
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2021-04-30 11:14:21 +0200

    theme: add color pallet

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -18,3 +18,49 @@ export type Fonts = {
       semiBold: 600;
     };
   };
+
+  export type BlueColors = {
+    blue100: "#65A1F8";
+    blue200: "#3584F6";
+    blue300: "#3869C2";
+    blue400: "#193E73";
+  };
+  
+  export type RedsColors = {
+    red100: "#E87D83";
+    red200: "#E04D55";
+    red300: "#C1374D";
+    red400: "#5C2023";
+  };
+  
+  export type GreensColors = {
+    green100: "#67B274";
+    green200: "#49A359";
+    green300: "#3A844B";
+    green400: "#25522D";
+  };
+  
+  export type OrangeColors = {
+    orange100: "#F3A077";
+    orange200: "#EF7E46";
+    orange300: "#D65D2A";
+    orange400: "#5C311B";
+  };
+  
+  export type GreysColors = {
+    grey50: "#FAFAFA";
+    grey100: "#F5F5F5";
+    grey150: "#F4F4F4";
+    grey200: "#E0E0E0";
+    grey300: "#BCBEBF";
+    grey350: "#636669";
+    grey400: "#3C3F43";
+    grey450: "#373A3E";
+    grey500: "#2C3034";
+  };
+  
+  export type BasicColors = {
+    white: "#FFFFFF";
+    black: "#212529";
+    transparent: "transparent";
+  };
